cbfbab1b686b1d80b92667c705349b20a21cdf37
yue.chang <lamplighter.planet@gmail.com>
2025-09-28 23:15:16 +0800

    init next project

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,6 @@ echo project: $project
 echo domain: $subDomain.$domain
 echo ====================================================================================
 
-# echo execute db scripts...
-# cd ./db
-# host=$(aws ssm get-parameter --name $env-db-host | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# user=$(aws ssm get-parameter --name $env-db-user | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# pwd=$(aws ssm get-parameter --name $env-db-pwd | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# cluster=$(aws ssm get-parameter --name $env-db-cluster | jq .Parameter.Value | sed -e 's/^"//' -e 's/"$//')
-# psql postgresql://$user:$pwd@$host:26257/$cluster.$project -f deploy.sql
-# echo ====================================================================================
-
 echo deploy backend AWS...
 cd ./backend
 npm install
@@ -36,9 +27,9 @@ echo ===========================================================================
 # cp -R src/constant ../webapp/src/constant/backend
 # echo ====================================================================================
 
-# echo deploy frontend to S3...
-# cd ../webapp
-# npm i
-# npm run build
-# aws s3 sync ./dist s3://$project-$env --delete --cache-control no-cache
-# echo ====================================================================================
+echo deploy frontend to S3...
+cd ../frontend
+npm i
+npm run build
+aws s3 sync ./out s3://$project-$env-y --delete --cache-control no-cache
+echo ====================================================================================
