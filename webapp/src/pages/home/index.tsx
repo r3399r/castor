5d88da2ed31a8d2027ff3cbe99d8c42ecb94706d
yue.chang <lamplighter.planet@gmail.com>
2026-03-30 23:04:09 +0800

    change text

--- webapp/src/pages/home/index.tsx
+++ webapp/src/pages/home/index.tsx
@@ -4,8 +4,8 @@ const Home = () => {
       <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">5 種類別</div>
       <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">10 個科目</div>
       <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">100 張試卷</div>
-      <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">1000 道試題</div>
-      <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">10000 名使用者</div>
+      <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">1,000 道試題</div>
+      <div className="w-4/5 cursor-pointer rounded-sm border p-2 text-center">300 名使用者</div>
     </div>
   );
 };
