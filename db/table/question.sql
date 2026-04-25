32a27b44b1276c0bf7b568ce1a6e9973ab8b8d2a
yue.chang <lamplighter.planet@gmail.com>
2026-04-26 02:59:47 +0800

    remove int length

--- db/table/question.sql
+++ db/table/question.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS castor.question (
     subject_id INT UNSIGNED NOT NULL,
     parent_id INT UNSIGNED NULL,
     fb_post_id VARCHAR(255) NULL,
-    is_group TINYINT(1) NOT NULL DEFAULT 0,
+    is_group TINYINT NOT NULL DEFAULT 0,
     type VARCHAR(255) NOT NULL, -- GROUP, SINGLE, MULTIPLE, TRUE_FALSE, FILL
     sort_order INT NULL, -- for GROUP
     content TEXT NULL,
