7bbd5416fb7e2860e2d4c594b95b2c71ad0e0d19
yue.chang <lamplighter.planet@gmail.com>
2026-05-11 23:20:55 +0800

    go

--- webapp/components/Navbar.tsx
+++ webapp/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     <nav className="rounded-[28px] border border-[#89776B] bg-[#FCF9F5] p-4 sm:p-6">
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <a href="/" className="font-helvetica text-lg font-bold tracking-[0.02em] text-[#2946B1]">
-          PMP.test
+          PMP.test2
         </a>
         <div className="flex flex-wrap items-center gap-2">
           {navItems.map((item) => (
