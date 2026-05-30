e6e8ebd5e5ad813de74737198962a1ba187c8b3d
axischang <91533060+axischang@users.noreply.github.com>
2026-05-30 12:33:11 +0800

    edit adaptive2 page's header

--- webapp/app/adaptive2/page.tsx
+++ webapp/app/adaptive2/page.tsx
@@ -4,8 +4,8 @@ import AdaptiveClient from './AdaptiveClient'
 
 export default function AdaptivePage() {
   return (
-    <div className="min-h-screen bg-beige-100 py-6">
-      <div className="mx-auto w-full max-w-[1200px] px-4 sm:px-6 lg:px-8">
+    <div className="m-[10px] min-h-[calc(100vh-20px)] border border-brown-700 bg-beige-100">
+      <div className="px-4 sm:px-6">
         <Navbar />
         <AuthGuard>
           <AdaptiveClient />
