50da124d52259d7348a4886867aa0810ae6cb28e
yue.chang <lamplighter.planet@gmail.com>
2026-07-25 23:54:12 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ echo ===========================================================================
 npm install
 
 echo deploy backend AWS...
-cd ./backend
+cd ./backend_new
 npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-ap-east-2
 aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway-ap-east-2 --capabilities CAPABILITY_NAMED_IAM
